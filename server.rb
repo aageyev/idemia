46f1ac12e8a4456a3f4cbe3f4ecd5c08105cdf96
Andrii Ageiev <a.ageyev@gmail.com>
2018-03-27 17:29:33 +0300

    added Logger to app

--- server.rb
+++ server.rb
@@ -1,16 +1,22 @@
 #!/usr/bin/env ruby
+
 require 'socket'
 require 'date'
 require 'json'
+require 'logger'
 
 # echo -n "[$(date +"%d/%m/%Y %H:%M")] Time to leave" | nc 127.0.0.1 1235
 
 (ENV['APP_LISTEN_ADDRESS'].nil? || ENV['APP_LISTEN_ADDRESS'].empty?) ? app_address = '0.0.0.0' : app_address = ENV['APP_LISTEN_ADDRESS']
 (ENV['APP_LISTEN_PORT'].nil? || ENV['APP_LISTEN_PORT'].empty?) ? app_port = 1234 : app_port = ENV['APP_LISTEN_PORT']
 
+# https://ruby-doc.org/stdlib-2.4.0/libdoc/logger/rdoc/Logger.html
+logger = Logger.new(STDOUT)
+logger.level = Logger::WARN
+
 server = TCPServer.new(app_address, app_port)
 begin
-  puts "Listening on #{app_address}:#{app_port}"
+  logger.warn("Listening on #{app_address}:#{app_port}")
   while connection = server.accept
     while line = connection.gets
       break if line =~ /quit/
@@ -22,15 +28,15 @@ begin
       # responce[:hostname] = datetime.to_i
       responce[:container] = `hostname`.strip
       responce[:message] = client_msg
-      puts line
+      logger.info(line)
       connection.puts "#{responce.to_json}\n"
     end
-    # connection.puts "Closing the connection. Bye!\n"
     connection.close
   end
 rescue SignalException => e
-  puts "Exiting server..."
+  logger.warn("Exiting server...")
 rescue Errno::ECONNRESET, Errno::EPIPE => e
   puts e.message
+  logger.fatal(e.message)
   retry
 end
